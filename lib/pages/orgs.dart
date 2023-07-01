44377dbb406c787f021bedaad035b197298540a9
Jesper Eriksson <84853548+jeppeerixon@users.noreply.github.com>
2023-07-01 14:19:17 +0200

    updated listview cards

--- lib/pages/orgs.dart
+++ lib/pages/orgs.dart
@@ -4,7 +4,12 @@ import 'package:flutter_app_1/pages/info.dart';
 class OrgScreen extends StatelessWidget {
   OrgScreen({ Key? key }) :super(key: key);
 
-  final List _clubs = ['Street Mentality', 'High Five TKD', 'Team Libaax', 'Forca Fighting'];
+  final List _clubs = [
+    // clubImage, clubName, clubCategory, clubRoom, clubUrl
+    ['lib/images/streetcard.jpg','Street Mentality', 'Akrobatik', 'Parkourhallen', 'www.streetmentality.se'],
+    ['lib/images/h5card.jpg','High Five Taekwondo', 'Kampsport', 'Röda', 'https://www.highfivetkd.se'],
+    ['lib/images/libaaxcard.jpg','Team Libaax', 'Träning', 'Blåsvarta', 'https://www.instagram.com/teamlibaax/'],
+    ];
 
   @override
   Widget build(BuildContext context) {
@@ -16,8 +21,14 @@ class OrgScreen extends StatelessWidget {
       body: ListView.builder(
         itemCount: _clubs.length,
         itemBuilder: (context, index) {
-          return CardContainer(child: _clubs[index],);
-        } )
+          return CardContainer(
+            clubImage: _clubs[index][0],
+            clubName: _clubs[index][1],
+            clubCategory: _clubs[index][2],
+            clubRoom: _clubs[index][3],
+            clubUrl: _clubs[index][4],
+          );
+        })
       ),
     );
   }
