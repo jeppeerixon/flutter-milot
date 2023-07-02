8f036cbca110f530406d84e4ef71010775f4373f
Jesper Eriksson <84853548+jeppeerixon@users.noreply.github.com>
2023-07-02 10:02:25 +0200

    final layout for organisations product cards

--- lib/pages/clubCard.dart
+++ lib/pages/clubCard.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
 
-class CardContainer extends StatelessWidget {  
-  final String clubImage; 
+class CardContainer extends StatelessWidget {
+
+  final String clubCategory;  
   final String clubName;
-  final String clubCategory;
+  final String clubDescription;   
   final String clubRoom; 
   final clubColor;
   final String clubUrl;
 
   const CardContainer({
     super.key,
-    required this.clubImage,
-    required this.clubName,
     required this.clubCategory,
+    required this.clubName,
+    required this.clubDescription,
     required this.clubRoom,
     required this.clubColor,
     required this.clubUrl
@@ -27,16 +28,31 @@ class CardContainer extends StatelessWidget {
           color: clubColor,
           borderRadius: BorderRadius.circular(10)
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(clubName),
-            Text(clubCategory),
-            Text(clubRoom),
-            Text(clubUrl),
-
-          ],
+        child: Padding(
+          padding: const EdgeInsets.only(left: 15.0, top: 7.5, right: 15.0, bottom: 7.5),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(clubCategory, style: TextStyle(color: Colors.black54)),
+              Text(clubName, style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500)),
+              Text(clubDescription),
+              Divider(),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('Hemsida', style: TextStyle(fontWeight: FontWeight.w500)),
+                  IconButton(
+                    icon: const Icon(Icons.arrow_circle_right),
+                    onPressed: () {
+                      print(clubUrl);
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/pages/orgs.dart
+++ lib/pages/orgs.dart
@@ -5,12 +5,16 @@ class OrgScreen extends StatelessWidget {
   OrgScreen({ Key? key }) :super(key: key);
 
   final List _clubs = [
-    // clubImage, clubName, clubCategory, clubRoom, clubUrl
-    ['lib/images/streetcard.jpg','Street Mentality', 'Akrobatik', 'Parkourhallen', Colors.purple, 'www.streetmentality.se'],
-    ['lib/images/h5card.jpg','High Five Taekwondo', 'Kampsport', 'Röda', Colors.red, 'https://www.highfivetkd.se'],
-    ['lib/images/libaaxcard.jpg','Team Libaax', 'Träning', 'Blåsvarta', Colors.blue, 'https://www.instagram.com/teamlibaax/'],
-    ['lib/images/forcacard.jpg','Forca Fighting', 'Kampsport', 'Blå', Colors.amber, 'https://www.forcafighting.com/'],
-    ];
+    // clubCategory, clubName, clubDescription, clubRoom, clubUrl
+    ['Akrobatik','Street Mentality', 'Träna Parkour, Freerunning & Tricking i Sveriges största parkourhall.' , 'Parkourhallen', Colors.purple, 'www.streetmentality.se'],
+    ['Kampsport','High Five Taekwondo', 'Taekwondo för alla åldrar och nivåer.', 'Röda', Colors.red, 'https://www.highfivetkd.se'],
+    ['Dans','Breakademics', 'Breakdance för barn samt vuxen drop-in', 'Orange', Colors.brown, 'https://www.sik.se'],
+    ['Träning','Team Libaax', 'Fysträning och bootcamps för alla olika åldrar, gratis alternativ finns.', 'Blåsvarta', Colors.blue, 'https://www.instagram.com/teamlibaax/'],
+    ['Kampsport','Tamashigoe Dojo', 'Självförsvar och kampkonst för barn som vuxna', 'Röda', Colors.teal, 'https://www.tamashigoe.com/'],
+    ['Kampsport','Forca Fighting', 'MMA, Thaiboxning och juijitsu för olika åldrar och nivåer.', 'Blå', Colors.amber, 'https://www.forcafighting.com/'],
+    ['Idrott','Ursvik IK', 'Innebandy och Basket för barn och ungdomar.', 'Orange', Colors.orange, 'https://www.ursvikik.se'],
+    ['Idrott','Sundbybergs IK', 'Orientering, Skidor, Fotboll och massor av andra idrotter.', 'Orange', Colors.green, 'https://www.sik.se'],
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -23,13 +27,14 @@ class OrgScreen extends StatelessWidget {
         padding: const EdgeInsets.all(10),
         itemCount: _clubs.length,
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          childAspectRatio: 1/1.25,
         crossAxisCount: 2,
         ),
         itemBuilder: (context, index) {
           return CardContainer(
-            clubImage: _clubs[index][0],
+            clubCategory: _clubs[index][0],
             clubName: _clubs[index][1],
-            clubCategory: _clubs[index][2],
+            clubDescription: _clubs[index][2],
             clubRoom: _clubs[index][3],
             clubColor: _clubs[index][4],
             clubUrl: _clubs[index][5],
